aaa73e0c09b90b922467fef05dfdc34bb42914e6
Amruta Misal <79842525+amruta-07@users.noreply.github.com>
2022-01-17 23:28:05 +0530

    Add files via upload

--- Arrays/CyclicallyRotateArray.java
+++ Arrays/CyclicallyRotateArray.java
@@ -1,1 +1,57 @@
+Cyclically rotate an array by one
 
+
+// { Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class GFG {
+	public static void main(String[] args) throws IOException
+	{
+	        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t =
+            Integer.parseInt(br.readLine().trim()); // Inputting the testcases
+        while(t-->0)
+        {
+            int n = Integer.parseInt(br.readLine().trim());
+            int a[] = new int[n];
+            // long getAnswer[] = new long[(int)(n)];
+            String inputLine[] = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) {
+                a[i] = Integer.parseInt(inputLine[i]);
+            }
+            
+            Compute obj = new Compute();
+            obj.rotate(a, n);
+            
+            StringBuilder output = new StringBuilder();
+            for(int i=0;i<n;i++)
+                output.append(a[i]+" ");
+            System.out.println(output);
+            
+        }
+	}
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Compute {
+    
+    public void rotate(int a[], int n)
+    {
+        for(int i=0;i<n-1;i++)
+       {
+           int temp=a[i];
+           a[i]=a[n-1];
+           a[n-1]=temp;
+       }
+        
+    }
+}
