e736a8e145a55483554c944f1cd9f00e2f410692
Amruta Misal <79842525+amruta-07@users.noreply.github.com>
2022-01-17 23:25:18 +0530

    Add files via upload

--- Arrays/RemoveDuplicate.java
+++ Arrays/RemoveDuplicate.java
@@ -1,1 +1,49 @@
+Remove Duplicate elements from sorted array
 
+// { Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+public class GFG {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        while(T>0)
+        {
+            int N = sc.nextInt();
+            int a[] = new int[N];
+            for(int i=0; i<N; i++)
+                a[i] = sc.nextInt();
+            
+            Solution g = new Solution();
+            int n = g.remove_duplicate(a,N);
+            
+            for(int i=0; i<n; i++)
+                System.out.print(a[i]+" ");
+            System.out.println();
+            T--;
+        }
+    }
+}// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution {
+   int remove_duplicate(int A[],int N){
+      
+   int i=0;
+       for(int j=1; j<N; j++)
+       {
+           if(A[j]!=A[i])
+           {
+               i++;
+               A[i] = A[j];
+           }
+       }
+       return i+1;
+   }
+   
+}
