46976914e6704c9e9bbedb6a2d1ef0239bf140f0
Amruta Misal <79842525+amruta-07@users.noreply.github.com>
2022-01-17 23:24:01 +0530

    Add files via upload

--- Arrays/UnionOfTwoArrays.java
+++ Arrays/UnionOfTwoArrays.java
@@ -1,1 +1,79 @@
+Union of two arrays
 
+
+// { Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+class GFG {
+	public static void main (String[] args) {
+	    
+	    //Taking input using class Scanner
+		Scanner sc=new Scanner(System.in);
+		
+		//Taking total count of testcases
+		int t=sc.nextInt();
+		sc.nextLine();
+		while(t-->0)
+		{
+		    
+		    int n,m;
+		    
+		    //taking size of array a
+		    n=sc.nextInt();
+		    
+		    //taking size of array b
+		    m=sc.nextInt();
+		    
+		    //Creating 2 array of size n and m
+		    int a[]=new int[n];
+		    int b[]=new int[m];
+		    
+		    //inserting elements to array a
+		    for(int i=0;i<n;i++)
+		    {
+		        a[i]=sc.nextInt();
+		    }
+		    
+		    //inserting elements to array b
+		    for(int i=0;i<m;i++)
+		    {
+		        b[i]=sc.nextInt();
+		    }
+		    Solution ob=new Solution();
+		    //calling doUnion method and printing the results
+		    System.out.println(ob.doUnion(a,n,b,m));
+		}
+		
+	}
+}// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution{
+   public static int doUnion(int a[], int n, int b[], int m) 
+   {   
+       //Only one loop and one HashSet.
+       HashSet<Integer> set1 = new HashSet<>();
+       
+       if(n>m){
+           for(int i=0;i<n;i++){
+               set1.add(a[i]);
+               if(i < m){
+                   set1.add(b[i]);
+               }
+           }
+       }else {
+           for (int i=0;i<m;i++){
+               if(i<n){
+                   set1.add(a[i]);
+               }
+               set1.add(b[i]);
+           }
+       }
+       
+       return set1.size();
+   }
+}
