d53c445cdfd16a1608c27a9539ed2d9d02f17cae
Amruta Misal <79842525+amruta-07@users.noreply.github.com>
2022-01-20 00:19:32 +0530

    Add files via upload

--- Arrays/BuySellStock.java
+++ Arrays/BuySellStock.java
@@ -1,1 +1,25 @@
+Best time to buy and sell stock
 
+
+
+class Solution {
+    public int maxProfit(int[] prices) {
+        int lsf = Integer.MAX_VALUE;
+        int op = 0;
+        int pist = 0;
+        
+        for(int i = 0; i < prices.length; i++){
+            if(prices[i] < lsf){
+                lsf = prices[i];
+            }
+            pist = prices[i] - lsf;
+            if(pist > op){
+                op = pist;
+            }
+        }
+        return op;
+    }
+        
+        
+    
+}
