8eeed35d4ff53b21fdba4d9f8096cb2c2b9b0f84
Amruta Misal <79842525+amruta-07@users.noreply.github.com>
2022-01-22 23:45:31 +0530

    Add files via upload

--- Arrays/ArraySubset.java
+++ Arrays/ArraySubset.java
@@ -1,1 +1,23 @@
+Array Subset of another array 
 
+
+class Compute {
+    public String isSubset( long a1[], long a2[], long n, long m) {
+        List<Long> list1 = new ArrayList<Long>();
+      for(long num:a1) {
+        list1.add(num);
+     }
+
+       int count = 0;
+       for(int i =0;i<m;i++)
+       {
+           if(!list1.contains(a2[i]))
+           {
+              return "No"; 
+           }
+       }
+       return "Yes";
+        
+        
+    }
+}
